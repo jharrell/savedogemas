053d2aa2f4644df0363b3794acd086bc25663c1a
jbharrell92 <jbharrell92@gmail.com>
2013-12-28 12:28:39 -0600

    first version of reimburse.py to use MySQL connection.

--- reimburse.py
+++ reimburse.py
@@ -4,58 +4,65 @@ reimbursement script version 0.1
 
 USAGE: python reimburse.py <address to send to> <amount to send>
 
-CAUTION: Make sure that the address listed as 'fromAddress' is the one being
-used by this script. ideally, this will be used on a computer with only one
-address, that being the one that will be filled to meet the needs of this script
+CAUTION: this script must be used on a computer with dogecoind running.
+amounts sent will be deducted from the wallet associated with dogecoind
 
 TODO: connect to MySQL database instead of using manual text entry.
 	  get dogecoind running on same server as MySQL database
 	  scrub all sensitive info so this can be open sourced :P
 
 """
-import sys, os, re, requests, jsonrpclib
+import sys, os, re, requests, jsonrpclib, MySQLdb
 
 from decimal import *
 
-access = jsonrpclib.Server("http://doge:wow@127.0.0.1:22555")
-
-
-if len(sys.argv) < 3:
-	sys.exit("Usage: %s <dogecoin address> <amount>" % sys.argv[0])
-
-#change this to whatever address we will actually be sending from
-#fromAddress = 'DCCpdXmwD9TjqnXvmm7NrrBQt2nBKEPDSt'
-fromAddress = 'DSqePEZM5xJmz6CcizkcSXnSTZkdRrYPE2'
-toAddress = sys.argv[1]
-amount = sys.argv[2]
 TRANSACTION_TAX = Decimal('1.0') # is this right?
 
-if type(amount) != Decimal:
-	amount = Decimal(amount)
+# set up necessary accesses...
+access = jsonrpclib.Server("http://doge:wow@127.0.0.1:22555")
+db = MySQLdb.connect("localhost","testuser","test123","TESTDB")
+cursor = db.cursor()
 
-#rounds so that there are no infinitely long numbers to deal with
-amount = amount.quantize(Decimal('0.000000001'))
-amount += TRANSACTION_TAX
+# get data from db...
+sql = "SELECT * FROM CLAIMS"
 
-fromValid = access.validateaddress(fromAddress)['isvalid']
-toValid = access.validateaddress(toAddress)['isvalid']
+try:
+	# Execute SQL command
+	cursor.execute(sql)
+	# Fetch ALL the rows!
+	results = cursor.fetchall()
+	for row in results:
+		toAddress = row[1]
+		amount = row[2]
+		if type(amount) != Decimal:
+			amount = Decimal(amount)
 
-if not (fromValid and toValid):
-	sys.exit('toAddress or fromAddress not valid')
+		#rounds so that there are no infinitely long numbers to deal with
+		amount = amount.quantize(Decimal('0.000000001'))
+		amount += TRANSACTION_TAX
 
-#get the balance of the supplying wallet
-balance = requests.get('http://dogechain.info/chain/Dogecoin/q/addressbalance/' + fromAddress)
+		toValid = access.validateaddress(toAddress)['isvalid']
 
+		if not toValid:
+			print 'toAddress or fromAddress not valid'
+			break
 
-#convert to decimal
-balance = Decimal(balance.text)
+		if amount > 50000:
+			print "Withdrawal amount: %d" % amount
+			print 'Requested withdrawal was too large'
+			break
 
-if amount >= balance or amount > 50000:
-	sys.exit('not enough funds in supplying wallet. Fill \'er up!')
+		try:
+			access.sendtoaddress(toAddress,float(amount))
+			print "SENDING: %d to %s" % (amount, toAddress)
+		except Exception, e:
+			print 'Inusfficient funds to complete transaction'
+			break
+except:
+	print "Error: unable to fetch data from db"
 
-print "BALANCE REMAINING: %d" % (balance - amount)
-print "SENDING: %d to %s" % (amount, toAddress)
-access.sendtoaddress(toAddress,float(amount))
+db.close()
+
 
 
 
