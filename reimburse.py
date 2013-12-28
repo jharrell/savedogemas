1dc507a87f07ec5d2a08324ee1b968a269d15056
jbharrell92 <jbharrell92@gmail.com>
2013-12-27 23:28:04 -0600

    script now correctly sends money to other account.

--- reimburse.py
+++ reimburse.py
@@ -1,24 +1,40 @@
+#!/usr/bin/python
+"""
+reimbursement script version 0.1
+
+USAGE: python reimburse.py <address to send to> <amount to send>
+
+CAUTION: Make sure that the address listed as 'fromAddress' is the one being
+used by this script. ideally, this will be used on a computer with only one
+address, that being the one that will be filled to meet the needs of this script
+
+TODO: connect to MySQL database instead of using manual text entry.
+	  get dogecoind running on same server as MySQL database
+	  scrub all sensitive info so this can be open sourced :P
+
+"""
 import sys, os, re, requests, jsonrpclib
 
 from decimal import *
 
 access = jsonrpclib.Server("http://doge:wow@127.0.0.1:22555")
-#print(access.getinfo())
+
 
 if len(sys.argv) < 3:
 	sys.exit("Usage: %s <dogecoin address> <amount>" % sys.argv[0])
 
-fromAddress = 'DCCpdXmwD9TjqnXvmm7NrrBQt2nBKEPDSt'
+#change this to whatever address we will actually be sending from
+#fromAddress = 'DCCpdXmwD9TjqnXvmm7NrrBQt2nBKEPDSt'
+fromAddress = 'DSqePEZM5xJmz6CcizkcSXnSTZkdRrYPE2'
 toAddress = sys.argv[1]
 amount = sys.argv[2]
-TRANSACTION_TAX = Decimal('0.001') # should be changed to actual number
+TRANSACTION_TAX = Decimal('1.0') # is this right?
 
 if type(amount) != Decimal:
 	amount = Decimal(amount)
 
 #rounds so that there are no infinitely long numbers to deal with
 amount = amount.quantize(Decimal('0.000000001'))
-
 amount += TRANSACTION_TAX
 
 fromValid = access.validateaddress(fromAddress)['isvalid']
@@ -37,5 +53,14 @@ balance = Decimal(balance.text)
 if amount >= balance or amount > 50000:
 	sys.exit('not enough funds in supplying wallet. Fill \'er up!')
 
-print balance
-print "SENDING: %d" % amount
+print "BALANCE REMAINING: %d" % (balance - amount)
+print "SENDING: %d to %s" % (amount, toAddress)
+access.sendtoaddress(toAddress,float(amount))
+
+
+
+
+
+
+
+
