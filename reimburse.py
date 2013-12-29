62d89970645793fec572bfea50c73f29d2ca0c75
jbharrell92 <jbharrell92@gmail.com>
2013-12-28 18:43:33 -0600

    script version 1.0 ready for integration with MySQL database

--- reimburse.py
+++ reimburse.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python
 """
-reimbursement script version 0.1
+The MIT License (MIT)
 
-USAGE: python reimburse.py <address to send to> <amount to send>
+Copyright (c) 2013 Jon Harrell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=======reimbursement script version 1.0==========
+
+USAGE: python reimburse.py
 
 CAUTION: this script must be used on a computer with dogecoind running.
 amounts sent will be deducted from the wallet associated with dogecoind
 
-TODO: connect to MySQL database instead of using manual text entry.
-	  get dogecoind running on same server as MySQL database
+MYSQL NOTE: rows should be row_id, toAddress(string), amount_reimbursed(decimal), amount_claimed(decimal).
+make sure that your table and this code matches. edit if necessary.
+
+TODO: get dogecoind running on same server as MySQL database
 	  scrub all sensitive info so this can be open sourced :P
 
 """
@@ -17,14 +40,15 @@ import sys, os, re, requests, jsonrpclib, MySQLdb
 from decimal import *
 
 TRANSACTION_TAX = Decimal('1.0') # is this right?
+REIMBURSEMENT_RATIO = Decimal('0.1') # change depending on amount raised
 
 # set up necessary accesses...
 access = jsonrpclib.Server("http://doge:wow@127.0.0.1:22555")
-db = MySQLdb.connect("localhost","testuser","test123","TESTDB")
+db = MySQLdb.connect(host="localhost",user="root",passwd="",db="testdb") # change this lol
 cursor = db.cursor()
 
 # get data from db...
-sql = "SELECT * FROM CLAIMS"
+sql = "SELECT * FROM verified_claims"
 
 try:
 	# Execute SQL command
@@ -32,31 +56,64 @@ try:
 	# Fetch ALL the rows!
 	results = cursor.fetchall()
 	for row in results:
+		row_id = row[0]
 		toAddress = row[1]
-		amount = row[2]
-		if type(amount) != Decimal:
-			amount = Decimal(amount)
+		amount_reimbursed = row[2]
+		amount_claimed = row[3]
+
+		#amount reimbursed and claimed should be stored as decimals
+		#to maintain precision.
+		if type(amount_reimbursed) != Decimal:
+			amount_reimbursed = Decimal(amount_reimbursed)
+		if type(amount_claimed) != Decimal:
+			amount_claimed = Decimal(amount_claimed)
+
+		ratio_reimbursed = ( amount_reimbursed / amount_claimed )
+		# Adding this to account for rounding errors, because you never know
+		ratio_reimbursed += Decimal("0.00000001") 
+
+		#check to see if already reimbursed
+		if ratio_reimbursed >= REIMBURSEMENT_RATIO:
+			print "Address %s has already been reimbursed %.2f percent! (%d / %d)" % (toAddress, ratio_reimbursed*100, amount_reimbursed, amount_claimed)
+			continue
+
+		owed = ( REIMBURSEMENT_RATIO * amount_claimed ) - amount_reimbursed
 
 		#rounds so that there are no infinitely long numbers to deal with
-		amount = amount.quantize(Decimal('0.000000001'))
-		amount += TRANSACTION_TAX
-
-		toValid = access.validateaddress(toAddress)['isvalid']
+		#not worried about this, as one Satoshi of a dogecoin makes no difference
+		owed = owed.quantize(Decimal('0.000000001'))
+		owed += TRANSACTION_TAX
+		try:
+			toValid = access.validateaddress(toAddress)['isvalid']
+		except:
+			print "Problem accessing dogecoind. Might not be running."
+			break
 
 		if not toValid:
-			print 'toAddress or fromAddress not valid'
-			break
+			print 'toAddress %s is not valid. Check row_id %d. Skipping reimbursement.' % (toAddress,row_id)
+			continue
 
-		if amount > 50000:
-			print "Withdrawal amount: %d" % amount
-			print 'Requested withdrawal was too large'
-			break
+		print "Owed amount: %d" % owed
+		if owed >= 50000:
+			print 'Requested withdrawal was too large (owed > 50000)'
+			print 'A problem may have occured'
+			continue
 
 		try:
-			access.sendtoaddress(toAddress,float(amount))
-			print "SENDING: %d to %s" % (amount, toAddress)
-		except Exception, e:
+			#access.sendtoaddress(toAddress,float(owed)) # converting to float loses precision, but is needed by sendtoaddress
+			print "SENDING: %d to %s" % (owed, toAddress)
+		except:
 			print 'Inusfficient funds to complete transaction'
+			break
+
+		amount_reimbursed += owed
+		sql_update = "UPDATE verified_claims SET amount_reimbursed = %s WHERE id = %s" % (amount_reimbursed, row_id)
+		try:
+			cursor.execute(sql_update)
+			db.commit()
+		except:
+			print "A problem occured during table update. Check row_id %d. New reimbursed should be %d" % (row_id, amount_reimbursed)
+			db.rollback()
 			break
 except:
 	print "Error: unable to fetch data from db"
