8e596a7193d63da39c2f1b57a480f15fc78c54f7
jbharrell92 <jbharrell92@gmail.com>
2013-12-30 16:39:11 -0600

    Updated header slightly, batched processing of sending DOGE and updating database, updated mysql requirements.

--- reimburse.py
+++ reimburse.py
@@ -21,18 +21,28 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-=======reimbursement script version 1.0==========
+=======reimbursement script version 1.1==========
 
 USAGE: python reimburse.py
 
-CAUTION: this script must be used on a computer with dogecoind running.
+DESCRIPTION: This is an auto-reimbursement script compatible with any bitcoin
+derived cryptocurrency that uses a similar structure to bitcoind. This script will
+1) Connect to a locally running instance of (in this case) dogecoind, or another altcoin
+2) Connect to a locally running MySQL database where the reimbursement amounts and addresses
+are held
+3) Iterate through the database and reimburse users up to a ratio defined by
+REIMBURSEMENT_RATIO. If an amount to reimburse is greater than a set amount (in this case 50,000)
+the user will be flagged and should be manually verified and dealt with instead of in this script.
+4) Database will be updated to reflect new amount paid to each claim.
+
+NOTE: this script must be used on a computer with dogecoind running.
 amounts sent will be deducted from the wallet associated with dogecoind
 
-MYSQL NOTE: rows should be id, toAddress(string), amount_reimbursed(decimal), amount_claimed(decimal), flag(tinyint or bool).
+MYSQL NOTE: rows should be id, toAddress(varchar 100), amount_reimbursed(decimal(22,9)), amount_claimed(decimal(22,9)), flag(tinyint or bool).
 make sure that your table and this code matches. edit if necessary.
 
-TODO: get dogecoind running on same server as MySQL database
-	  scrub all sensitive info so this can be open sourced :P
+TODO: 	further check code for vulnerabilities and inefficiencies.
+		scrub all sensitive info so this can be open sourced :P
 
 """
 import sys, os, re, requests, jsonrpclib, MySQLdb
@@ -41,6 +51,7 @@ from decimal import *
 
 TRANSACTION_TAX = Decimal('1.0') # is this right?
 REIMBURSEMENT_RATIO = Decimal('0.15') # change depending on amount raised
+REIMBURSEMENT_CAP = 50000
 
 # set up necessary accesses...
 access = jsonrpclib.Server("http://doge:wow@127.0.0.1:22555")
@@ -86,7 +97,7 @@ try:
 		try:
 			toValid = access.validateaddress(toAddress)['isvalid']
 		except:
-			print "Problem accessing dogecoind. Might not be running."
+			print "Problem accessing dogecoind."
 			break
 
 		if not toValid:
@@ -94,8 +105,8 @@ try:
 			continue
 
 		print "Owed amount: %d" % owed
-		if owed >= 50000:
-			print 'Requested withdrawal was too large (owed > 50000). Updating flag.'
+		if owed >= REIMBURSEMENT_CAP:
+			print 'Requested withdrawal was too large (owed > %d). Updating flag.' % REIMBURSEMENT_CAP
 			sql_flag = "UPDATE verified_claims SET flag = 1 WHERE id = %s" % row_id
 			try:
 				cursor.execute(sql_flag)
@@ -106,32 +117,33 @@ try:
 				break
 			continue
 
-		try:
-			#access.sendtoaddress(toAddress,float(owed)) # converting to float loses precision, but is needed by sendtoaddress
-			print "SENDING: %d to %s" % (owed, toAddress)
-		except:
-			print 'Inusfficient funds to complete transaction'
-			break
 
-		amount_reimbursed += owed
-		sql_update = "UPDATE verified_claims SET amount_reimbursed = %s WHERE id = %s" % (amount_reimbursed, row_id)
+		#IMPORTANT BLOCK: sending DOGE and updating DB in one try catch block. if either fail, program halts and 
+		#user is informed of the location that corrections need to be made to.
 		try:
+			amount_reimbursed += owed
+			sql_update = "UPDATE verified_claims SET amount_reimbursed = %s WHERE id = %s" % (amount_reimbursed, row_id)
+
+			print "SENDING: %d to %s" % (owed, toAddress)
+			#access.sendtoaddress(toAddress,float(owed)) # converting to float loses precision, but is needed by sendtoaddress
+			print "UPDATING DB"
 			cursor.execute(sql_update)
-			db.commit()
 		except:
-			print "Error occured while updating amount_reimbursed. Check row_id %d. New reimbursed should be %d" % (row_id, amount_reimbursed)
 			db.rollback()
+			print 'An error occured.'
+			print '======INFORMATION======'
+			print 'Address: %s' % toAddress
+			print 'amount to send: %d' % owed
+			print 'row_id: %d' % row_id
+			#as amount_reimbursed has been updated, this is what the amount_reimbursed column should be at. 
+			#check dogechain.info logs to see if owed was sent.
+			print 'total amt to be reimbursed: %d' % amount_reimbursed
+			print '======================='
 			break
 except:
 	print "Error: unable to fetch data from db"
 
 db.close()
-
-
-
-
-
-
 
 
 
